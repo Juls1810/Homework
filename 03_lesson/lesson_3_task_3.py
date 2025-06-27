17e174dd94b5badbad7fc93bbcd8f5e4ea6bcc94
Juls1810 <Ladi1993@mail.ru>
2025-06-27 14:20:07 +0300

    add lesson3

---
+++ 03_lesson/lesson_3_task_3.py
@@ -0,0 +1,11 @@
+from Address import Address
+from Mailing import Mailing
+
+Mailing = Mailing
+
+to_address = 348000, "г. Липецк", "ул. Свиридова", 14, 240
+from_address = 258000, "г. Воронеж", "ул. Пушкина", 5, 100
+track = 125896378
+cost = 12000
+
+print("Отправление", track, "из", from_address, "в", to_address, ". Стоимость", cost, "рублей.")
