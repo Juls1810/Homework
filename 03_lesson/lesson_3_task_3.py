19ecb1ec3fc2bde85cef91a21a9ffb9a9674481f
Juls1810 <Ladi1993@mail.ru>
2025-07-03 15:42:29 +0300

    correction of task 3

--- 03_lesson/lesson_3_task_3.py
+++ 03_lesson/lesson_3_task_3.py
@@ -2,11 +2,11 @@ from Address import Address
 from Mailing import Mailing
 
 
-to_address = 348000, "г. Липецк", "ул. Свиридова", 14, 240
-from_address = 258000, "г. Воронеж", "ул. Пушкина", 5, 100
-track = 125896378
+to_address = Address(348000, "г. Липецк", "ул. Свиридова", 14, 240)
+from_address = Address(258000, "г. Воронеж", "ул. Пушкина", 5, 100)
+track = "125896378"
 cost = 12000
 
-Mailing = Mailing
+Mailing = Mailing(to_address, from_address, cost, track)
 
-print("Отправление", track, "из", from_address, "в", to_address, ". Стоимость", cost, "рублей.")
+print(f"Отправление {Mailing.track} из {Mailing.from_address} в {Mailing.to_address}. Стоимость {Mailing.cost} рублей.")
