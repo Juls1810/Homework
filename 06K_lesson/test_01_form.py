2d837d3de4da28500cf37bdcfb0390d8d44bbbff
Juls1810 <Ladi1993@mail.ru>
2025-07-28 14:30:02 +0300

    chang

--- 06K_lesson/test_01_form.py
+++ 06K_lesson/test_01_form.py
@@ -1,27 +1,29 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome()
-driver.maximize_window()
 
-driver.get("https://bonigarcia.dev/selenium-webdriver-java/data-types.html")
-driver.find_element(By.NAME, "first-name").send_keys("Иван")
-driver.find_element(By.NAME, "last-name").send_keys("Петров")
-driver.find_element(By.NAME, "address").send_keys("Ленина, 55-3")
-driver.find_element(By.NAME, "city").send_keys("Москвa")
-driver.find_element(By.NAME, "country").send_keys("Россия")
-driver.find_element(By.NAME, "e-mail").send_keys("test@skypro.com")
-driver.find_element(By.NAME, "phone").send_keys("+7985899998787")
-driver.find_element(By.NAME, "job-position").send_keys("QA")
-driver.find_element(By.NAME, "company").send_keys("SkyPro")
-driver.find_element(By.XPATH, "//button[text()='Submit']").click()
+def test_form():
+    driver = webdriver.Chrome()
+    driver.maximize_window()
 
-pole_z = driver.find_element(By.ID, "zip-code").get_attribute("class")
-assert pole_z == "alert py-2 alert-danger"
+    driver.get("https://bonigarcia.dev/selenium-webdriver-java/data-types.html")
+    driver.find_element(By.NAME, "first-name").send_keys("Иван")
+    driver.find_element(By.NAME, "last-name").send_keys("Петров")
+    driver.find_element(By.NAME, "address").send_keys("Ленина, 55-3")
+    driver.find_element(By.NAME, "city").send_keys("Москвa")
+    driver.find_element(By.NAME, "country").send_keys("Россия")
+    driver.find_element(By.NAME, "e-mail").send_keys("test@skypro.com")
+    driver.find_element(By.NAME, "phone").send_keys("+7985899998787")
+    driver.find_element(By.NAME, "job-position").send_keys("QA")
+    driver.find_element(By.NAME, "company").send_keys("SkyPro")
+    driver.find_element(By.XPATH, "//button[text()='Submit']").click()
 
-poles = ["#first-name", "#last-name", "#address", "#city", "#country", "#e-mail", "#phone", "#company"]
-for pole in poles:
+    pole_z = driver.find_element(By.ID, "zip-code").get_attribute("class")
+    assert pole_z == "alert py-2 alert-danger"
+
+    poles = ["#first-name", "#last-name", "#address", "#city", "#country", "#e-mail", "#phone", "#company"]
+    for pole in poles:
         pole_class = driver.find_element(By.CSS_SELECTOR, pole).get_attribute("class")
         assert pole_class == "alert py-2 alert-success"
 
-driver.quit()
+    driver.quit()
